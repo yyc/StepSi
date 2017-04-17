2c6441dd8f661974785ac55bd32e681e460a2098
Yuchuan <yyc478@gmail.com>
2017-04-17 17:35:38 +0800

    Add files to replicate GATTack

---
+++ hackSteps.js
@@ -0,0 +1,23 @@
+var utils = require('../lib/utils');
+
+var stepAmount = "00ffff"; // Make sure these are 6 hex characters
+
+function lockNotify(peripheralId, service, characteristic, type, data, notifyEmitter, callback){
+//	console.log('    lock notify hook'.yellow);
+	var datastr = data.toString('hex');
+
+
+// Extract the parts of the string we want to keep
+	var strpart1 = datastr.substr(0, 10);
+	var strpart2 = datastr.substring(18);
+
+	//Add in the step count
+	var data = strpart1 + stepAmount + strpart2;
+	console.log(`Received         ${datastr}, overwriting with ${data}`
+
+	callback(null, new Buffer(data,'hex'));
+
+}
+
+
+module.exports.lockNotify = lockNotify;
